c02e787b5d61b5bf257876e8257e28a7ba5c7938
SaintRoguer <anonymousvsworld@gmail.com>
2022-08-07 11:03:06 -0300

    Changed how images are fetched

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -10,18 +10,23 @@ const MovieList = (props) => {
     props.setActive(movie)
   }
 
+
+  async function getImageFromSlug(slug) {
+    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/arts/${slug}/image`)
+    return res.data
+  }
+
   return (
     <>
       {props.movies.map((movie, index) =>
         <div key={movie.slug ? movie.slug : movie.id} className='div__poster' >
           <img className='row__poster'
-            src={movie.slug ? process.env.REACT_APP_BASE_URL + `/arts/${movie.slug}/image` : movie.image}
+            src={movie.slug ? getImageFromSlug(movie.slug) : movie.image}
             height="216" width="321"
             alt={movie.title}
             onClick={() => changeState(movie)}
           />
         </div>)
-
       }
 
     </>
