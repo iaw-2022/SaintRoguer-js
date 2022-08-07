d18789fc899fe6ab40690abb14a29dd45a2f7efa
SaintRoguer <anonymousvsworld@gmail.com>
2022-08-07 13:31:20 -0300

    sw update

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,12 @@
 /* eslint-disable no-restricted-globals */
 import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
-import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
-
-clientsClaim();
-
+import { CacheFirst } from "workbox-strategies";
 
 // Puedes desactivar el precaching reemplazand esta línea
 precacheAndRoute(self.__WB_MANIFEST);
-// por esta otra:
-// const desactivarPrecache = self.__WB_MANIFEST;
-// para más info: https://cra.link/PWA
-
-
 
 registerRoute(
     ({ event }) => event.request.destination === 'image',
@@ -60,52 +52,4 @@ registerRoute(
     })
 );
 
-
-const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
-registerRoute(
-    // Return false to exempt requests from being fulfilled by index.html.
-    ({ request, url }) => {
-        // If this isn't a navigation, skip.
-        if (request.mode !== "navigate") {
-            return false;
-        } // If this is a URL that starts with /\_, skip.
-        if (url.pathname.startsWith("/_")) {
-            return false;
-        } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-        if (url.pathname.match(fileExtensionRegexp)) {
-            return false;
-        } // Return true to signal that we want to use the handler.
-        return true;
-    },
-    createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
-);
-//Cache all images
-
-
-/*
-
-registerRoute(
-    // Add in any other file extensions or routing criteria as needed.
-    ({ url }) =>
-        url.origin === self.location.origin && (url.pathname.endsWith(".png") || url.pathname.endsWith(".jpg")), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-    new StaleWhileRevalidate({
-        cacheName: "images",
-        plugins: [
-            // Ensure that once this runtime cache reaches a maximum size the
-            // least-recently used images are removed.
-            new ExpirationPlugin({ maxAgeSeconds: 60 * 60 * 24 * 15 }),
-        ],
-    })
-);
-*/
-
-
-
-
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
-});
 
