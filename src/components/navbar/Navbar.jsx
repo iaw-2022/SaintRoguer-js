c30d9ae58850c8426506b6512f8ea28a250b33bd
SaintRoguer <anonymousvsworld@gmail.com>
2022-08-10 10:13:01 -0300

    Trying to fix firebase comp

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -28,6 +28,26 @@ const Navbar = () => {
         });
     }, []);
 
+    async function downloadApp() {
+        console.log("👍", "butInstall-clicked");
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+            // The deferred prompt isn't available.
+            console.log("oops, no prompt event guardado en window");
+            return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        const result = await promptEvent.userChoice;
+        console.log("👍", "userChoice", result);
+        // Reset the deferred prompt variable, since
+        // prompt() can only be called once.
+        window.deferredPrompt = null;
+        // Hide the install button.
+        setIsReadyForInstall(false);
+    }
+
     return (
         <div className="navbar">
             <div className="navbar-links">
--- src/firebase.js
+++ src/firebase.js
@@ -15,4 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const messaging = firebase.messaging.isSupported() ? getMessaging(app) : null;
+export const messaging = app.messaging.isSupported() ? getMessaging(app) : null;
